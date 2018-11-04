678b09952b90698daaa6591ed9d39104be3115b3
Umer Abbasi <rajaumerabbasi@gmail.com>
2018-11-04 23:20:53 +1100

    v1

---
+++ test_jenkins/src/test/java/test_jenkins/test_jenkins/AppTest.java
@@ -0,0 +1,29 @@
+package test_jenkins.test_jenkins;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
+public class AppTest 
+{
+    
+    @Test
+    public void test() throws InterruptedException
+    {
+		System.setProperty("webdriver.gecko.driver","/Users/umerabbs/Documents/workspace/testJava/geckodriver");
+		WebDriver driver = new FirefoxDriver();
+		
+		String appUrl = "http://google.com";
+	    driver.get(appUrl);
+	    
+	    driver.findElement(By.name("q")).sendKeys("Download Selenium");
+	    //Thread.sleep(3000L);
+	    driver.findElement(By.name("btnK")).click();
+	    
+	    driver.close();
+
+
+    }
+}
